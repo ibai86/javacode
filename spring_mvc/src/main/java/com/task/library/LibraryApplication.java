4cadfc4588fcb45c45503ab1bbb176af10d3ec39
Ivan Baev <ibai86@yandex.ru>
2025-09-07 23:08:00 +0300

    add main app

---
+++ spring_mvc/src/main/java/com/task/library/LibraryApplication.java
@@ -0,0 +1,26 @@
+package com.task.library;
+
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class LibraryApplication {
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.task.store.config");
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8082);
+        tomcat.getConnector();
+
+        org.apache.catalina.Context servletContext = tomcat.addContext("", null);
+
+        Tomcat.addServlet(servletContext, "dispatcherServlet", servlet).setLoadOnStartup(1);
+        servletContext.addServletMappingDecoded("/", "dispatcherServlet");
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
