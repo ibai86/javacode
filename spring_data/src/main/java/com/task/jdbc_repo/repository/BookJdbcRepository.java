a8cb2c6e616deb8b8dfa6d7df7e3ad9741f4ecf3
Ivan Baev <ibai86@yandex.ru>
2025-09-10 01:31:54 +0300

    add BookJdbcRepository

---
+++ spring_data/src/main/java/com/task/jdbc_repo/repository/BookJdbcRepository.java
@@ -0,0 +1,29 @@
+package com.task.jdbc_repo.repository;
+
+import com.task.jdbc_repo.model.Book;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookJdbcRepository {
+
+    Book save(Book book);
+
+    Book update(Book book);
+
+    Optional<Book> findById(Long id);
+
+    void delete(Long id);
+
+    List<Book> findAll();
+
+    default RowMapper<Book> getRowMapper() {
+        return (resultSet, rowNum) -> Book.builder()
+                .id(resultSet.getLong("id"))
+                .title(resultSet.getString("title"))
+                .author(resultSet.getString("author"))
+                .publicationYear(resultSet.getInt("publication_year"))
+                .build();
+    }
+}
