34bc3ab18e6cf8b3e9ba8639700291777d93db95
Ivan Baev <ibai86@yandex.ru>
2025-09-10 03:13:27 +0300

    add save method

--- spring_data/src/main/java/com/task/jdbc_repo/repository/BookRepository.java
+++ spring_data/src/main/java/com/task/jdbc_repo/repository/BookRepository.java
@@ -2,23 +2,39 @@ package com.task.jdbc_repo.repository;
 
 import com.task.jdbc_repo.model.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class BookRepository implements BookJdbcRepository {
-
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insert;
 
-    public BookRepository(JdbcTemplate jdbcTemplate) {
+    public BookRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.insert = new SimpleJdbcInsert(dataSource)
+                .withTableName("books")
+                .usingGeneratedKeyColumns("id");
     }
 
     @Override
     public Book save(Book book) {
-        return null;
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("title", book.getTitle());
+        params.put("author", book.getAuthor());
+        params.put("publication_year", book.getPublicationYear());
+
+        Number newId = insert.executeAndReturnKey(params);
+        book.setId(newId.longValue());
+
+        return book;
     }
 
     @Override
