57b0db0105f0388ba6a5b5b41026e0c70ec1e95b
Ivan Baev <ibai86@yandex.ru>
2025-09-10 12:02:22 +0300

    add mainApp

--- com.data.jdbc/src/main/java/task/jdbc/repo/JdbcApplication.java
+++ com.data.jdbc/src/main/java/task/jdbc/repo/JdbcApplication.java
@@ -1,4 +1,27 @@
 package task.jdbc.repo;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 public class JdbcApplication {
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.task.library.config");
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context servletContext = tomcat.addContext("", null);
+
+        Tomcat.addServlet(servletContext, "dispatcherServlet", servlet).setLoadOnStartup(1);
+        servletContext.addServletMappingDecoded("/", "dispatcherServlet");
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
